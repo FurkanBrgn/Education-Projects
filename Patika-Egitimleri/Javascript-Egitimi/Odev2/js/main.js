793a071b12a569ded22f7ce533833d08c4466ff7
Furkan Bağırgan <furkanbrgn@gmail.com>
2022-03-14 17:58:14 +0300

    Update main.js

--- Patika-Egitimleri/Javascript-Egitimi/Odev2/js/main.js
+++ Patika-Egitimleri/Javascript-Egitimi/Odev2/js/main.js
@@ -1,5 +1,8 @@
+//Get the todo-list
 var myNodelist = document.getElementsByTagName("LI");
+
 var i;
+//create span and li and append todo-list
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -8,6 +11,7 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
+//create close action for todo
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -17,6 +21,7 @@ for (i = 0; i < close.length; i++) {
   };
 }
 
+//create click event for todo
 var list = document.querySelector("ul");
 list.addEventListener(
   "click",
@@ -28,6 +33,7 @@ list.addEventListener(
   false
 );
 
+//create new todo function
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("task").value;
